c92db2d2cfc9101cc25599a79bb55bef768a33fd
Joaquim Rodrigues <jmscrodrigues@hotmail.com>
2020-10-22 11:11:03 +0100

    Added agency and agent data

--- src/RealEstateAgency.java
+++ src/RealEstateAgency.java
@@ -1,5 +1,41 @@
 import jade.core.Agent;
 
 public class RealEstateAgency extends Agent{
+    private List<RealEstateAgent> agents;
+    private float minAgentRate;
+    private float maxAgentRate;
+    private int teamSize;
+
+    RealEstateAgency(int size, float minAgentRate, float maxAgentRate, int teamSize) {
+        agents = new ArrayList<RealEstateAgent> agents;
+        this.minAgentRate = minAgentRate;
+        this.maxAgentRate = maxAgentRate;
+    }
+
+    public int getTeamSize() {
+        return agents.size();
+    }
+
+    public void addAgent(RealEstateAgent agent) {
+        if (this.getTeamSize() < this.teamSize) {
+            agents.add(agent);
+        }
+    }
+
+    public int getMaxSize() {
+        return this.teamSize;
+    }
+
+    public void removeAgent (RealEstateAgency agent) {
+        agents.remove(agent);
+    }
+
+    public float getAgentMinRate() {
+        return this.minAgentRate;
+    }
+
+    public float getMaxAgentRate() {
+        retutn this.maxAgentRate;
+    }
 
 }
--- src/RealEstateAgent.java
+++ src/RealEstateAgent.java
@@ -1,5 +1,19 @@
 import jade.core.Agent;
 
 public class RealEstateAgent extends Agent{
+    private RealEstateAgency agency = null;
+    private float agentRate;
 
+    RealEstateAgent(float agentRate){
+        this.agentRate = agentRate;
+    }
+
+    public void setAgency(RealEstateAgency agency) {
+        this.agency = agency;
+    }
+
+    public void removeAgency() {
+        this.agency = null;
+    }
+    
 }
