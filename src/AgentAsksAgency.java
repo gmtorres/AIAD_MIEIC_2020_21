9915353846e91f98e8fa90742e3b7ef3412ac278
Joaquim Rodrigues <jmscrodrigues@hotmail.com>
2020-11-06 15:55:04 +0000

    Started Proposer protocol for agencies and agents

---
+++ src/AgentAsksAgency.java
@@ -0,0 +1,31 @@
+import jade.proto.ProposeInitiator;
+
+import java.util.Vector;
+
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.UnreadableException;
+
+
+public class AgentAsksAgency extends ProposeInitiator {
+	
+	private RealEstateAgent agent;
+	
+	
+	public AgentAsksAgency(RealEstateAgent agent ,ACLMessage cfp) {
+		super(agent, cfp);
+		this.agent = agent;
+	}
+	
+	protected Vector<ACLMessage> prepareCfps(ACLMessage cfp) {
+		Vector<ACLMessage> v = new Vector<ACLMessage >();
+		cfp.setContent("Quero entrar na agencia! A minha taxa é:" + agent.getAgentRate());
+		DFAgentDescription[] agencies = this.agent.searchForAgency();
+		for (int i = 0; i < agencies.length; i++) {
+			//String name = agencies[i].getName().getName();
+			cfp.addReceiver(agencies[i].getName());
+		}
+		v.add(cfp);
+		return v;		
+	}
+}
--- src/RealEstateAgent.java
+++ src/RealEstateAgent.java
@@ -1,4 +1,9 @@
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+
 import java.util.Random;
 
 public class RealEstateAgent extends Agent{
@@ -21,5 +26,19 @@ public class RealEstateAgent extends Agent{
     public int getAgentRate() {
     	return this.agentRate;
     }
+    
+    public DFAgentDescription[] searchForAgency() {
+		DFAgentDescription template = new DFAgentDescription();
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("RealEstateAgency");
+		template.addServices(sd);
+		DFAgentDescription[] result = null;
+		try {
+			result = DFService.search(this, template);
+		}catch(FIPAException fe) {
+			fe.printStackTrace();
+		}
+		return result;
+	}
 
 }
